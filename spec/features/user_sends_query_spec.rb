88b51ab57c7c56a424ab557e24cae96b851572d1
Balazs Koskoci <koskoci.balazs@gmail.com>
2021-07-26 11:45:37 +0200

    Add feature spec

---
+++ spec/features/user_sends_query_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "User sends query", type: :feature do
+  scenario "blank search is made" do
+    visit root_path
+    
+    click_on "Search"
+    
+    expect(page).not_to have_css '#results'
+    expect(page).to have_css '#not-found', text: "Sorry, no recipes found with these ingredients"
+  end
+  
+  scenario "no recipe found" do
+    visit root_path
+    
+    fill_in "query", with: "oeuf, viande, foobarbaz"
+    click_on "Search"
+    
+    expect(page).not_to have_css '#results'
+    expect(page).to have_css '#not-found', text: "Sorry, no recipes found with these ingredients"
+  end
+  
+  scenario "recipes found" do
+    visit root_path
+    
+    fill_in "query", with: "oeuf, viande"
+    click_on "Search"
+    
+    expect(page).to have_css '#results'
+    expect(page).not_to have_css '#not-found'
+  end
+end
