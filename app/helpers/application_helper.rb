da4322ce76317330b157b4380260cdfb69e8ca32
Balazs Koskoci <koskoci.balazs@gmail.com>
2021-07-26 11:47:23 +0200

    Move connection helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def conn
+    @conn ||= if ENV['DATABASE_URL'].present?
+      uri = URI.parse(ENV['DATABASE_URL'])
+      PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+    else
+      PG.connect(dbname: 'postgres')
+    end
+  end
 end
